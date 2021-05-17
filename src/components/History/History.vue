d2112cb926fcf53351c0ed3195f0ab8283f2ca4a
TheOnlySlothman <lasselm013@gmail.com>
2021-05-17 13:12:31 +0200

    i hate merging

--- src/components/History/History.vue
+++ src/components/History/History.vue
@@ -1,39 +1,27 @@
 <template>
     <div>
         <!-- History component consisting of multiple HistoryOrders -->
-<<<<<<< HEAD
          <history-order
         v-for="(x, index) in hOrder"
         :key="index"
         :hOrderProduct="x">
         </history-order>
-=======
->>>>>>> master
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import HistoryOrder from './HistoryOrder.vue'
 export default {
     components: { HistoryOrder },
     props: {
         hOrder: Array
     }
-=======
-export default {
-
->>>>>>> master
 }
 </script>
 
 <style>
-<<<<<<< HEAD
 template{
     width:200px;
     border:1px solid;
 }
-=======
-
->>>>>>> master
 </style>
